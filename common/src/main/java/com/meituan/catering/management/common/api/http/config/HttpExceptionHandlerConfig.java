8c14f7adb4485621a64c76f79b09e3676731a7e4
杜林峰 <dulinfeng@meituan.com>
2021-11-30 15:33:17 +0800

    支持全局异常在API层的捕获和处理

--- common/src/main/java/com/meituan/catering/management/common/api/http/config/HttpExceptionHandlerConfig.java
+++ common/src/main/java/com/meituan/catering/management/common/api/http/config/HttpExceptionHandlerConfig.java
@@ -11,23 +11,30 @@ import java.util.Map;
 
 /**
  * 公共Web异常拦截器
+ *
+ * @author dulinfeng
  */
 @ControllerAdvice
 public class HttpExceptionHandlerConfig {
 
     @ExceptionHandler(IllegalArgumentException.class)
     public ResponseEntity<String> handleIllegalArgumentException(IllegalArgumentException ex) {
-        return buildGlobalModelAndView(ex);
+        return buildGlobalModelAndView(ex, HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(IllegalStateException.class)
     public ResponseEntity<String> handleIllegalStateException(IllegalStateException ex) {
-        return buildGlobalModelAndView(ex);
+        return buildGlobalModelAndView(ex, HttpStatus.BAD_REQUEST);
     }
 
-    private ResponseEntity<String> buildGlobalModelAndView(RuntimeException ex) {
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleException(Exception ex) {
+        return buildGlobalModelAndView(ex, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    private ResponseEntity<String> buildGlobalModelAndView(Exception ex, HttpStatus httpStatus) {
         return ResponseEntity
-                .status(HttpStatus.BAD_REQUEST)
+                .status(httpStatus)
                 .body(ex.getMessage());
     }
 
